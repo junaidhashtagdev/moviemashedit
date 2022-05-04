e0ef3649d5ce462a20bd39fa7ca899657af3df3b
ajstudd <j7654894110@gmail.com>
2022-05-04 18:20:45 +0530

    necessary changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { StrictMode } from "react";
+import "./App.css";
+import {
+  ApiClient,
+  Masher,
+  DefaultMasherProps,
+} from "@moviemasher/client-react";
+import "@moviemasher/client-react/dist/moviemasher.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const applicationOptions = { previewSize: { width: 480, height: 270 } };
+  const options = DefaultMasherProps(applicationOptions);
+  const masher = <Masher {...options} />;
+  const editor = <ApiClient>{masher}</ApiClient>;
+  const strictMode = <StrictMode>{editor}</StrictMode>;
+  return <div className="app">{strictMode}</div>;
 }
 
 export default App;
